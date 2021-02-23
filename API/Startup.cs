070dcbb475a03e6d394f62d2f0430295ee619aa6
= <=>
2021-02-23 16:22:22 -0500

    Section 19, before switching DB

--- API/Startup.cs
+++ API/Startup.cs
@@ -75,11 +75,15 @@ namespace API
             app.UseAuthentication();
             app.UseAuthorization();
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
                 endpoints.MapHub<PresenceHub>("hubs/presence");
                 endpoints.MapHub<MessageHub>("hubs/message");
+                endpoints.MapFallbackToController("Index", "FallBack");
             });
         }
     }
