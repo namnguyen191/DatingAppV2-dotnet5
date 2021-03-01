e975a7d44d4f5ae54579e1ce6fa20116d3fe813e
= <=>
2021-02-28 21:20:22 -0500

    Fix problem in API

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -74,7 +74,7 @@ namespace API.Controllers
 
             var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
 
-            if (!result.Succeeded) return Unauthorized();
+            if (!result.Succeeded) return Unauthorized("Invalid Password");
 
             return new UserDto
             {
